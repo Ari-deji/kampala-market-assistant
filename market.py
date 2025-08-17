cfab078a5bf7e8f11428fdacc5cce1076ae75af3
ari-deji <dakmud@gmail.com>
2025-08-17 12:07:33 -0400

    Updates to initial script

--- market.py
+++ market.py
@@ -42,65 +42,65 @@ shopping_list = ["beets", "arugula", "spinach", "spinach", "apples", "okra", "po
 
 # Dictionary to track purchased items for the bill
 
-purchased_items = {}
+# purchased_items = {}
 
 # Initialising the total cost variable
 
-total_cost = 0
+# total_cost = 0
 
 # Adding control flow to process the shopping list - Part 2
 # Another attempt at the for loop
 
-for item in shopping_list: # Check for availability of the item
-    if item in produce: # Check for stock quantities
-        if stock[item] > 0:
-            total_cost = total_cost + produce[item] 
+# for item in shopping_list: # Check for availability of the item
+    # if item in produce: # Check for stock quantities
+        # if stock[item] > 0:
+            # total_cost = total_cost + produce[item] 
 
     # Reduce stock items
 
-    stock[item] = stock[item] - 1
+    # stock[item] = stock[item] - 1
 
-        else:  # This item is sold here, but they're all out
+        # else:  
+            # print(f"{item} is out of stock!")
 
-        print(f"{item} is out of stock!")
+    # else: # When item is not in my list
+        # print(f"{item} not found in inventory!")
 
-    else: # When item is not in my list
-        print(f"{item} not found in inventory!")
-
-    print(f"Final Total Cost:" {total_cost})
-    print("Remaining Stock:", stock)
+    # print(f"Final Total Cost:" {total_cost})
+    # print("Remaining Stock:", stock)
         
 # Calculate and return final bill
 
-print("\n" +"="*30)
-print("       Final Bill is")
-print("="*30)
+# print("\n" +"="*30)
+# print("       Final Bill is")
+# print("="*30)
 
 # Return each purchased item, its quantity and line total
 
-for item in purchased_items:
+# for item in purchased_items:
 
-    quantity = purchased_items[item]  # Get the quantity value
+    # quantity = purchased_items[item]  # Get the quantity value
 
-    price_per_item = produce[item]  # Get the price of one item
+    # price_per_item = produce[item]  # Get the price of one item
 
-    line_price = price_per_item * quantity  # Calculate the total line item
+    # line_price = price_per_item * quantity  # Calculate the total line item
 
-    print(f"- {item.capitalize()}(x{quantity}: {line_total} UShs)")  # This line I had to google :) 
+    # print(f"- {item.capitalize()}(x{quantity}: {line_total} UShs)")  # This line I had to google :) 
 
-print("="*30)
+# print("="*30)
 
 # Print totals
 
-print(f"Grand total:     {total_cost}  UShs")
-print("="*30)
-print("Thank you for your purchase!")
+# print(f"Grand total:     {total_cost}  UShs")
+# print("="*30)
+# print("Thank you for your purchase!")
             
 # Part 3 - with reusable functions and return results like walked through above - not adding comments to explain
 
+
 def calculate_bill(inventory, customer_list):
     """
-        Processes a customer's shopping list against the store's inventory, then calcualtes and prints the final bill
+        Processes a customer's shopping list against the store's inventory, then calculates and prints the final bill
     """
 total_cost = 0
 purchased_items = {}
@@ -112,19 +112,20 @@ print("Processing your shopping list....")
 for item in customer_list:
     if item in inventory:
         if inventory[item]["stock"] > 0:
-        total_cost = total_cost + inventory[item]['price']
-        inventory[item]["stock"] = inventory[item]["stock"] - 1
+            price = inventory[item]["price"]
+            total_cost = total_cost + inventory[item]['price']
+            inventory[item]["stock"] = inventory[item]["stock"] - 1
 
-        if item in purchased_items:
-            purchased_items[item] = purchased_items[item] + 1
-        else:
-            purchased_items[item]  = 1
+            if item in purchased_items:
+                purchased_items[item] = purchased_items[item] + 1
+            else:
+                purchased_items[item]  = 1  
 
         else:
-        print(f"Note: {item}  is out of stock!")
+            print("Note:",  item,  "is out of stock!")
 
     else:
-        print(f"Note: {item} is not sold here!")
+        print("Note:", item, "is not sold here!")
 
 print("\n" + "=" * 30)
 print("       Final Bill is")
@@ -136,6 +137,9 @@ for item, quantity in purchased_items.items():
     line_total = inventory[item]["price"] * quantity
     print(f"- {item.capitalize()} (x{quantity}): {line_total} UShs")  
 
-print("="*30)
-print(f"Grand Total: {total_cost} UShs")
+print("=" * 30)
+print("Grand Total:", total_cost, " UShs")
 print("-"*30)
+
+calculate_bill(produce, shopping_list)
+
